2851edf1dbb149453d711768af932d9d5441d876
Martiks38 <cappa.giuliano@gmail.com>
2023-08-29 13:41:13 -0300

    refactor: aplicar cambios funcionales a getItem y a getQuery

--- src/services/getItem.ts
+++ src/services/getItem.ts
@@ -1,66 +1,105 @@
-import { item, description } from '@/__mock__/item.js'
+import { BASE_URL_API, baseResponse } from '@/assets/consts'
+import type { ItemData, ResponseError, ResponseItem } from '@/typing'
 
-type ItemData = {
-	condition: string
-	description: string
-	free_shipping: boolean
-	id: string
-	picture: string
-	price: {
-		currency: string
-		amount: number
-		decimals: number
-	}
-	sold_quantity: number
-	title: string
+interface Snapshot {
+	url: string
+	width: number
+	height: number
+	status: string
 }
 
-type ResponseItemData = {
-	author: {
-		name: string
-		lastname: string
-	}
-	item: ItemData
+interface ItemDescription {
+	text: string
+	plain_text: string
+	last_updated: string
+	date_created: string
+	snapshot: Snapshot
 }
 
-export async function getItem(): Promise<ResponseItemData> {
-	const {
-		id,
-		currency_id,
-		price,
-		pictures,
-		condition,
-		shipping,
-		sold_quantity,
-		title,
-	} = item
+/**
+ *  generateItemURL devuelve un arreglo de la URL con los datos del producto o ítem y la URL de la descripción del producto o ítem
+ */
 
-	// Arreglo de los segmentos del precio conteniendo el valor entero y pudiendo tener valores decimales
-	const priceSegments: string[] = price.toString().split('.')
-	const decimals = priceSegments.length === 2 ? Number(priceSegments.at(-1)) : 0
+const generateItemURL = (item_id: string) => {
+	const itemURL = `${BASE_URL_API}/items/${item_id}`
+	const itemDescriptionURL = `${itemURL}/description`
 
-	const itemData = {
-		author: {
-			name: 'Giuliano',
-			lastname: 'Cappa',
-		},
-		item: {
-			condition,
-			free_shipping: shipping.free_shipping,
-			description: description.text || description.plain_text,
+	return [itemURL, itemDescriptionURL]
+}
+
+export async function getItem(
+	item_id: string
+): Promise<ResponseItem | ResponseError> {
+	let responseApi: ResponseItem | ResponseError
+
+	try {
+		const responses = await Promise.all(
+			generateItemURL(item_id).map((url) => fetch(url))
+		)
+		const dataItem: unknown[] = await Promise.all(
+			responses.map((response) => response.json())
+		)
+
+		const [data, description] = dataItem
+
+		if (!responses[0].ok) {
+			throw new Error('Se produjo un error al intentar obtener el producto')
+		}
+
+		if (!responses[1].ok) {
+			throw new Error(
+				'Se produjo un error al intentar obtener la descripción del producto'
+			)
+		}
+
+		const {
 			id,
-			picture: pictures[0].url || pictures[0].secure_url,
-			price: {
-				currency: currency_id,
-				amount: price,
-				decimals,
-			},
+			currency_id,
+			price,
+			pictures,
+			condition,
+			shipping,
 			sold_quantity,
 			title,
-		},
+		} = data as ItemData
+
+		// Arreglo de los segmentos del precio conteniendo el valor entero y pudiendo tener valores decimales
+		const priceSegments: string[] = price.toString().split('.')
+		const decimals =
+			priceSegments.length === 2 ? Number(priceSegments.at(-1)) : 0
+
+		const { plain_text, text } = description as ItemDescription
+
+		const itemData = {
+			...baseResponse,
+			item: {
+				condition,
+				free_shipping: shipping.free_shipping,
+				description: text || plain_text,
+				id,
+				picture: pictures[0].url || pictures[0].secure_url,
+				price: {
+					currency: currency_id,
+					amount: price,
+					decimals,
+				},
+				sold_quantity,
+				title,
+			},
+		}
+
+		responseApi = itemData
+	} catch (error: unknown) {
+		responseApi = {
+			...baseResponse,
+			message:
+				error instanceof Error
+					? error.message
+					: 'Se produjo un error al obtener los datos del producto',
+		}
 	}
 
 	return new Promise((res, rej) => {
-		setTimeout(() => res(itemData), 1000)
+		setTimeout(() => res(responseApi), 1000)
 	})
 }
--- src/services/getQuery.ts
+++ src/services/getQuery.ts
@@ -1,76 +1,86 @@
-import { mockSearch } from '@/__mock__/search.js'
+import { BASE_URL_API, baseResponse } from '@/assets/consts'
 
-type Item = {
-	id: string
-	title: string
-	price: {
-		currency: string
-		amount: number
-		decimals: number
-	}
-	picture: string
-	condition: string
-	free_shipping: boolean
-	city: string
-}
-
-export type QueryResult = {
-	author: {
-		name: string
-		lastname: string
-	}
-	items: Item[]
-}
+import type {
+	Item,
+	ItemProduct,
+	ItemQuery,
+	MercadoLibreResponse,
+	ResponseError,
+	ResponseQuery,
+} from '@/typing'
 
 const maxQuantityItems = 4
 
-export async function getQuery(): Promise<QueryResult> {
-	const { results } = mockSearch
+export async function getQuery(
+	query: string
+): Promise<ResponseQuery | ResponseError> {
+	let response: ItemQuery[] | string
 
-	/* No pude encontrar las categorías en la respuesta de la api */
-	const mappedResults = results.slice(0, maxQuantityItems).map((res) => {
-		const {
-			id,
-			title,
-			price,
-			currency_id,
-			thumbnail,
-			condition,
-			shipping,
-			seller_address,
-		} = res
+	try {
+		const resp = await fetch(`${BASE_URL_API}/sites/MLA/search?q=${query}`)
 
-		// Arreglo de los segmentos del precio conteniendo el valor entero y pudiendo tener valores decimales
-		const priceSegments: string[] = price.toString().split('.')
-		const decimals =
-			priceSegments.length === 2 ? Number(priceSegments.at(-1)) : 0
+		const data: MercadoLibreResponse = await resp.json()
 
-		return {
-			id,
-			title,
-			price: {
-				currency: currency_id,
-				amount: price,
-				decimals,
-			},
-			picture: thumbnail,
-			condition,
-			free_shipping: shipping.free_shipping,
-			city: seller_address.city.name,
+		if (!resp.ok) {
+			throw new Error('Se produjo un error durante la búsqueda')
 		}
-	})
+
+		if (data.results.length === 0) {
+			throw new Error('No se hayaron resultados para la búsqueda')
+		}
+
+		const mappedResults = data.results.slice(0, maxQuantityItems).map((res) => {
+			const {
+				id,
+				title,
+				price,
+				currency_id,
+				thumbnail,
+				condition,
+				shipping,
+				seller_address,
+			} = res
+
+			// Arreglo de los segmentos del precio conteniendo el valor entero y pudiendo tener valores decimales
+			const priceSegments: string[] = price.toString().split('.')
+			const decimals =
+				priceSegments.length === 2 ? Number(priceSegments.at(-1)) : 0
+
+			return {
+				id,
+				condition,
+				city: seller_address.city.name,
+				free_shipping: shipping.free_shipping,
+				picture: thumbnail,
+				price: {
+					currency: currency_id,
+					amount: price,
+					decimals,
+				},
+				title,
+			}
+		})
+
+		response = mappedResults
+	} catch (error: unknown) {
+		const isInstanceofError = error instanceof Error
+
+		response = isInstanceofError
+			? error.message
+			: 'Se produjo un error durante la búsqueda'
+	}
 
 	return new Promise((res, rej) => {
-		setTimeout(
-			() =>
-				res({
-					author: {
-						name: 'giuliano',
-						lastname: 'cappa',
-					},
-					items: mappedResults,
-				}),
-			1000
-		)
+		setTimeout(() => {
+			const resp: Record<'message', string> | Record<'items', ItemQuery[]> =
+				typeof response === 'string'
+					? { message: response }
+					: { items: response }
+
+			res({
+				...baseResponse,
+				...resp,
+			})
+		}, 1000)
 	})
 }
